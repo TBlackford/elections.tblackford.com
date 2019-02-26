2e78fbdc2b293b3afeb2c65b5666ff159cf9661f
unknown <iamtomblackford@gmail.com>
2019-02-26 13:09:13 +1300

    added group by function in elections route

--- server/routes/elections.js
+++ server/routes/elections.js
@@ -7,6 +7,43 @@ const router     = express.Router();
 
 module.exports = router;
 
+////////////////////////////////////////////////////////////
+// Helper route functions
+
+const groupByElections = (elections) => {
+    var previousYear = elections[0].year;
+    var newElections = {};
+    
+    for(var i in elections) {
+        if(previousYear != elections[i].year) {
+            previousYear = elections[i].year;
+        }
+
+        if(!newElections[previousYear]) {
+            newElections[previousYear] = [];
+        }      
+        
+        newElections[previousYear].push(elections[i]);
+    }
+
+    elections = [];
+    for(var i in newElections) {
+        elections.push(newElections[i]);
+    }
+
+    return elections;
+}
+
+const groupBy = (group, elections) => {
+    console.log(group);
+    switch(group) {
+        case "year":
+            return groupByElections(elections);
+        default:
+            return elections;
+    }
+}
+
 const send = (res, err, elections) => {
     if (err) {
         res.status(400).send({ message: 'Get election failed', err });
@@ -19,6 +56,9 @@ const send = (res, err, elections) => {
     }
 }
 
+////////////////////////////////////////////////////////////
+// Adding to the db
+
 router.get('/', (req, res) => {
     Election.find({}, { __v: 0 }, (err, elections) => {
         send(res, err, elections);
@@ -26,7 +66,9 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:country', (req, res) => {
+    // First, find the country
     Country.findOne({
+        // making sure that the ISO code is uppercase
         isoCode: req.params.country.toUpperCase()
     }, { __v: 0 }, (err, country) => {
         if (err) {
@@ -35,6 +77,13 @@ router.get('/:country', (req, res) => {
             Election.find({
                 country: country._id
             }, { _id: 0, __v: 0 }, (err, elections) => {
+                // Check for queries
+                if(req.query.groupby) {
+                    // The group by query
+                    elections = groupBy(req.query.groupby, elections);
+                }
+
+                // Send the data
                 send(res, err, elections);
             }).populate('country').sort({ year: -1, votingSystem: 1 });   
         }
@@ -42,16 +91,20 @@ router.get('/:country', (req, res) => {
 });
 
 router.get('/:country/:year', (req, res) => {
+    // First, find the country
     Country.findOne({
-        isoCode: req.params.country.toUpperCase()
+        // making sure that the ISO code is uppercase
+        isoCode: req.params.country.toUpperCase() 
     }, { __v: 0 }, (err, country) => {
         if (err) {
             res.status(400).send({ message: 'Get country failed', err });
         } else {
+            // Find the election with the found country ID
             Election.find({
                 country: country._id,
                 year: req.params.year
             }, { _id: 0, __v: 0 }, (err, elections) => {
+                // Send the data
                 send(res, err, elections);
             }).populate('country').sort({year: -1, votingSystem: 1});   
         }
